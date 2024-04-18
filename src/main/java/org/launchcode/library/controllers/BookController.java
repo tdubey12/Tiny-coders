4a0595a16812e94a29eb6357b08d96ad56848910
anicraj456 <anicraj123@gmail.com>
2024-04-18 17:40:51 -0500

    updated

--- src/main/java/org/launchcode/library/controllers/BookController.java
+++ src/main/java/org/launchcode/library/controllers/BookController.java
@@ -1,12 +1,12 @@
 package org.launchcode.library.controllers;
 
 import jakarta.validation.Valid;
-import org.launchcode.library.models.Book;
-import org.launchcode.library.models.BookCheckout;
-import org.launchcode.library.models.BookData;
-import org.launchcode.library.models.BooksInventory;
+import org.launchcode.library.models.*;
 import org.launchcode.library.models.data.BookCheckoutRepository;
 import org.launchcode.library.models.data.BookRepository;
+import org.launchcode.library.models.data.StudentBookRepository;
+import org.launchcode.library.models.data.StudentRepository;
+import org.launchcode.library.models.dto.StudentBookDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 @RequestMapping("books")
@@ -27,9 +23,12 @@ public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
-
+    @Autowired
+    private StudentRepository studentRepository;
     @Autowired
     private BookCheckoutRepository bookCheckoutRepository;
+    @Autowired
+    private StudentBookRepository studentBookRepository;
 
     //codes for search book by Anitha
     static HashMap<String, String> bookSearchOptions = new HashMap<>();
@@ -47,7 +46,7 @@ public class BookController {
     public String index(Model model) {
         //anitha added booksearchoptions
         model.addAttribute("columns", bookSearchOptions);
-        model.addAttribute("title", "All Books");
+        //model.addAttribute("title", "All Books");
         model.addAttribute("books", bookRepository.findAll());
         return "books/index";
     }
@@ -55,7 +54,7 @@ public class BookController {
     @GetMapping("/search")
     public String search(Model model) {
         model.addAttribute("columns", bookSearchOptions);
-        model.addAttribute("title", "All Books");
+        //model.addAttribute("title", "All Books");
         model.addAttribute("books", bookRepository.findAll());
         return "books/index";
     }
@@ -83,6 +82,7 @@ public class BookController {
         return "books/view";
     }
 
+    //add a book
     @GetMapping("/add") //http://localhost:8080/books/add
     public String displayAddBookForm(Model model) {
 
@@ -106,6 +106,8 @@ public class BookController {
         //added redirect route to success message
        // return "redirect:/books/add?success";
     }
+
+    //update a book
     @GetMapping("/update") //http://localhost:8080/books/update?bookId=xx
     public String displayUpdateBookForm(@RequestParam Integer bookId, Model model){
         model.addAttribute("title", "Update book");
@@ -148,6 +150,7 @@ public class BookController {
         return "redirect:/books/update";
 
     }
+    //details of the book
     @GetMapping("detail") //http://localhost:8080/books/detail?bookId=xx
     public String displayBookDetail(@RequestParam Integer bookId, Model model){
         Optional<Book> result = bookRepository.findById(bookId);
@@ -157,6 +160,7 @@ public class BookController {
         return "books/detail";
     }
 
+    //delete by id
     @GetMapping("deletelist") //http://localhost:8080/books/deletelist
     public String displayDeleteBooksForm(Model model) {
         model.addAttribute("title", "Delete Books");
@@ -174,6 +178,8 @@ public class BookController {
         }
         return "redirect:";
     }
+
+    //delete
     @GetMapping("delete") //http://localhost:8080/books/delete?bookId=xx
     public String displayBookDelete(@RequestParam Integer bookId, Model model){
         Optional<Book> result = bookRepository.findById(bookId);
@@ -186,19 +192,20 @@ public class BookController {
 
     @PostMapping("delete") //http://localhost:8080/books/delete?bookId=xxxx
     public String processDeleteBook(@RequestParam(required = true) Integer bookId) {
-
         if (bookId != null) {
-
+            //Anitha: add one line code to remove the hold if there is a hold already on the book by the student
+            removeHoldWhileCheckingOut(bookId);
             bookRepository.deleteById(bookId);
         }
         return "redirect:";
     }
+
+    //checkout
     @GetMapping("checkout") //http://localhost:8080/books/checkout
     public String displayCheckout(Model model, int bookId){
         model.addAttribute(new BookCheckout());
         model.addAttribute("bookId",bookId);
         model.addAttribute("title", "Checkout book");
-
         return "books/checkout";
     }
 
@@ -223,11 +230,11 @@ public class BookController {
         return "redirect:";
     }
 
+    //Checkin
     @GetMapping("checkin") //http://localhost:8080/books/checkin
     public String displayCheckin(Model model, int bookId){
         model.addAttribute("bookId",bookId);
         model.addAttribute("title", "Checkin book");
-
         return "books/checkin";
     }
 
@@ -246,5 +253,63 @@ public class BookController {
         return "redirect:";
     }
 
+    //Anitha code for hold a book
+    @GetMapping("hold/{bookId}")
+    public String displayHold(@PathVariable("bookId")Integer bookId,Model model) {
 
+        Optional<Book> optionalBook = bookRepository.findById(bookId);
+        Book book = optionalBook.get();
+        /**if(book.getAvailableCopiesToIssue() == 0){
+            model.addAttribute("message","Copies available, do checkout instead of hold");
+            return "books/hold";
+        }**/
+        StudentBookDto studentBookDto = new StudentBookDto();
+        studentBookDto.setBookId(bookId);
+        studentBookDto.setBookName(book.getName());
+        model.addAttribute("studentBookDto",studentBookDto);
+        model.addAttribute("allstudents",studentRepository.findAll());
+        return "books/hold";
+    }
+    //Anitha code for hold a book
+    @PostMapping("hold")
+    public String processHold(@ModelAttribute @Valid StudentBookDto studentBookDto,
+                              Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "books/hold";
+        }
+        int studentId = studentBookDto.getStudentId();
+        Optional<Book> result = bookRepository.findById(studentBookDto.getBookId());
+        Book book = result.get();
+        StudentBook studentBook = new StudentBook();
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        Student student = studentOptional.get();
+        StudentBookId studentBookId = new StudentBookId();
+        studentBookId.setBookId(book.getId());
+        studentBookId.setStudentId(student.getId());
+        studentBook.setId(studentBookId);
+        studentBook.setStudent(student);
+        studentBook.setBook(book);
+        studentBook.setIssueDate(studentBookDto.getIssueDate());
+        studentBook.setHeldUntilDate(studentBookDto.getHeldUntilDate());
+        studentBook.setCheckOut(false);
+        studentBook.setHold(true);
+        int availableCopies = book.getAvailableCopiesToIssue();
+        studentBookRepository.save(studentBook);
+        book.setAvailableCopiesToIssue(availableCopies--);
+        bookRepository.save(book);
+        return "redirect:";
+
+    }
+
+    //Anitha code for deleting book on hold
+    //gets all record from studentBook and check if bookId is equals to getBook and getId then it will delete that book
+    private void removeHoldWhileCheckingOut(int bookId){
+        Iterable<StudentBook> studentBookList = studentBookRepository.findAll();
+        for (StudentBook studentBook : studentBookList){
+            if(studentBook.getBook().getId() == bookId){
+                studentBookRepository.delete(studentBook);
+            }
+        }
+
+    }
 }
